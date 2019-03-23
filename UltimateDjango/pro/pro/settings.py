46d91eba9d447faf4501c0ea3a5965999b65fba4
siva <sivanarayana93@gmail.com>
2019-03-23 21:44:12 +0530

    static folder created

--- UltimateDjango/pro/pro/settings.py
+++ UltimateDjango/pro/pro/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Marketing',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Marketing',
+    
 ]
 
 ROOT_URLCONF = 'pro.urls'
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'), 
+)
